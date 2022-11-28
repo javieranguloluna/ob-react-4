270a0a2971b5703189908a7aeb000ba82bd9f17a
javieranguloluna <54935581+javieranguloluna@users.noreply.github.com>
2022-11-28 12:57:03 +0100

    events & loop

--- src/components/cuadrado.jsx
+++ src/components/cuadrado.jsx
@@ -1,17 +1,43 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
 
 const Cuadrado = () => {
 
     const [style, setStyle] = useState({
         width: '255px',
         height: '255px',
-        backgroundColor: 'black'
+        backgroundColor: 'black',
+        transition: 'background-color 0.5s ease-in-out'
     })
 
-    const ref = useRef()
+    const [loop, setLoop] = useState()
+
+    const setColor = () => {
+        setStyle({
+            ...style,
+            backgroundColor: `rgb(${Math.random() * 256},${Math.random() * 256},${Math.random() * 256})`
+        })
+    }
+
+    const handleMouseOver = (e) => {
+        setLoop(setInterval(setColor,500))
+    }
+
+    const handleMouseLeave = (e) => {
+        clearInterval(loop)
+    }
+
+    const handleDoubleClick = (e) => {
+        clearInterval(loop)
+    }
+
 
     return (
-        <div ref={ref} style={style}></div>
+        <div
+            onMouseOver={handleMouseOver}
+            onMouseLeave={handleMouseLeave}
+            onDoubleClick={handleDoubleClick}
+            style={style}>
+        </div>
     )
 }
 
